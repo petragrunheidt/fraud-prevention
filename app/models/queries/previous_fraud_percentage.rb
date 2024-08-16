1f9c144317f2369f188115bd0f5df2ba5a830477
petra <petra.grunheidt@rebase.com.br>
2024-08-16 20:48:40 -0300

    changes query to return normalized result

---
+++ app/models/queries/previous_fraud_percentage.rb
@@ -0,0 +1,38 @@
+class Queries::PreviousFraudPercentage
+  def initialize(user_id, device_id, merchant_id)
+    @user_id = user_id
+    @device_id = device_id
+    @merchant_id = merchant_id
+  end
+
+  def self.normalized_previous_fraud_percentage(user_id, device_id, merchant_id)
+    new(user_id, device_id, merchant_id).normalized_previous_fraud_percentage
+  end
+
+  def normalized_previous_fraud_percentage
+    percentages = previous_fraud_list_percentages.values
+
+    percentages.sum / percentages.size.to_f
+  end
+
+  private
+
+  attr_reader :user_id, :device_id, :merchant_id
+
+  def previous_fraud_list_percentages
+    { user_id:, device_id:, merchant_id: }.each_with_object({}) do |(k, v), acc|
+      fraud_percentage = attribute_frauds_percentage({ k => v })
+      acc[k] = fraud_percentage
+    end
+  end
+
+  def attribute_frauds_percentage(attribute)
+    result = Transaction.where(attribute)
+                        .group(:has_cbk)
+                        .count
+
+    return 0.0 if result.values.sum.zero? || result[true].nil?
+
+    (result[true].to_f / result.values.sum)
+  end
+end
---
+++ spec/queries/previous_fraud_percentage_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Queries::PreviousFraudPercentage do
+  describe '.previous_fraud_list_percentages' do
+    let(:user_id) { 100 }
+    let(:device_id) { 200 }
+    let(:merchant_id) { 300 }
+
+    before do
+      create(:transaction, user_id:, device_id:, merchant_id:,
+                           has_cbk: true)
+      create(:transaction, user_id:, device_id:, merchant_id:,
+                           has_cbk: false)
+      create_list(:transaction, 3, user_id:, has_cbk: true)
+      create_list(:transaction, 3, device_id:, has_cbk: false)
+      create_list(:transaction, 3, merchant_id:, has_cbk: true)
+    end
+
+    it 'returns the correct fraud percentages for each attribute' do
+      normalized_percentage = described_class
+                              .normalized_previous_fraud_percentage(user_id, device_id, merchant_id)
+
+      expect(normalized_percentage).to eq(0.6)
+    end
+
+    it 'when only one attribute matches a previous transaction' do
+      normalized_percentage = described_class
+                              .normalized_previous_fraud_percentage(user_id, 202, 303)
+
+      expect(normalized_percentage).to be 0.8 / 3.0
+    end
+
+    it 'returns 0.0 if no transactions for an attribute' do
+      normalized_percentage = described_class
+                              .normalized_previous_fraud_percentage(101, 202, 303)
+
+      expect(normalized_percentage).to eq(0.0)
+    end
+  end
+end
