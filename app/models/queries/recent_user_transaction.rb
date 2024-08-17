4d4402c9035205449a421b852855073915e71e89
petra <petra.grunheidt@rebase.com.br>
2024-08-17 20:31:25 -0300

    adds recent transactions count query to recent_user_transactions

--- app/models/queries/recent_user_transaction.rb
+++ app/models/queries/recent_user_transaction.rb
@@ -1,8 +1,14 @@
 module Queries
   class RecentUserTransaction
-    def initialize(user_id, transaction_date)
+    DEFAULT_RECENT_THRESHOLD = 12.hours
+    def initialize(
+      user_id,
+      transaction_date,
+      recent_threshold = DEFAULT_RECENT_THRESHOLD
+    )
       @user_id = user_id
       @transaction_date = transaction_date
+      @recent_threshold = recent_threshold
     end
 
     def self.time_since_last_transaction(user_id:, transaction_date:)
@@ -13,9 +19,24 @@ module Queries
       latest_transaction_date && (transaction_date - latest_transaction_date)
     end
 
+    def self.recent_transactions_count(
+      user_id:,
+      transaction_date:,
+      recent_threshold: DEFAULT_RECENT_THRESHOLD
+    )
+      new(user_id, transaction_date, recent_threshold).recent_transactions_count
+    end
+
+    def recent_transactions_count
+      Transaction.where(
+        user_id:,
+        transaction_date: transaction_date.ago(recent_threshold)..transaction_date
+      ).count
+    end
+
     private
 
-    attr_reader :user_id, :transaction_date
+    attr_reader :user_id, :transaction_date, :recent_threshold
 
     def latest_transaction_date
       Transaction.where(user_id:)
--- spec/queries/recent_user_transaction_spec.rb
+++ spec/queries/recent_user_transaction_spec.rb
@@ -30,4 +30,31 @@ RSpec.describe Queries::RecentUserTransaction do
       expect(result).to be_within(0.1).of Time.zone.now - (Time.zone.now - 6.hours)
     end
   end
+
+  context '.recent_transactions_count' do
+    let(:user_id) { 10 }
+    before do
+      [1, 2, 3, 4, 16, 20, 24, 28].each do |i|
+        FactoryBot.create(:transaction, user_id:, transaction_date: Time.zone.now - i.hours)
+      end
+    end
+
+    it 'returns the number of transactions in the last 12 hours (default)' do
+      transactions_count = described_class
+                           .recent_transactions_count(user_id:, transaction_date: Time.zone.now)
+
+      expect(transactions_count).to eq 4
+    end
+
+    it 'returns the number of transactions within a custom treshold' do
+      transactions_count = described_class
+                           .recent_transactions_count(
+                             user_id:,
+                             transaction_date: Time.zone.now,
+                             recent_threshold: 18.hours
+                           )
+
+      expect(transactions_count).to eq 5
+    end
+  end
 end
