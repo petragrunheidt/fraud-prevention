f5e62ca75efde866a1f525333bf674d8de1d0d25
petra <petra.grunheidt@rebase.com.br>
2024-08-17 19:15:01 -0300

    adjusts response's recommendation with new method and minor refactoring

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,13 @@
 class TransactionsController < ApplicationController
   def create
-    transaction = Transaction.new(
-      transaction_date: Time.zone.now,
-      **transaction_params
-    )
-    transaction.has_cbk = flag_fraud? transaction
+    transaction = Transaction.new(transaction_params)
+    is_fraud = flag_fraud?(transaction)
+    transaction.has_cbk = is_fraud
 
     return :unprocessable_entity unless transaction.save!
 
-    response = { transaction_id: transaction.transaction_id, recommendation: 'approve' }
+    response = { transaction_id: transaction.transaction_id,
+                 recommendation: recommendation(is_fraud) }
 
     render json: response, status: :ok
   end
@@ -21,6 +20,10 @@ class TransactionsController < ApplicationController
     fraud_score > 15
   end
 
+  def recommendation(is_fraud)
+    is_fraud ? 'deny' : 'approve'
+  end
+
   def transaction_params
     params
       .require(:transaction)
@@ -30,6 +33,6 @@ class TransactionsController < ApplicationController
         :card_number,
         :transaction_amount,
         :device_id
-      )
+      ).merge({ transaction_date: Time.zone.now })
   end
 end
