c2de1f6858a135a824e9b2cae8b4389e598195c1
petra <petra.grunheidt@rebase.com.br>
2024-08-18 11:52:29 -0300

    adds model validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+  validates :transaction_amount,
+            :transaction_date,
+            :user_id,
+            :merchant_id,
+            :card_number,
+            presence: true
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  it 'true' do
-    expect(true).to be_truthy
+  context 'when validating attributes' do
+    %i[
+      transaction_amount
+      transaction_date
+      user_id
+      merchant_id
+      card_number
+    ].each do |attribute|
+      it { is_expected.to validate_presence_of(attribute) }
+    end
   end
 end
